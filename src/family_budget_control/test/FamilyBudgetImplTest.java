17ebde2384ad7a061804460e3def623bea597741
german <german.kisin.box@gmail.com>
2024-11-18 18:01:54 +0100

    Test changed1

--- src/family_budget_control/test/FamilyBudgetImplTest.java
+++ src/family_budget_control/test/FamilyBudgetImplTest.java
@@ -89,7 +89,7 @@ class FamilyBudgetImplTest {
     void testOutcomeByProduct() {
         List<Outcome> products = familyBudget.outcomeByProduct("products");
         assertNotNull(products); // Убедиться, что результат не null
-        assertEquals(2, products.size()); // Проверить количество расходов
+        assertEquals(2, products.size());
 
         // Проверить, что все результаты принадлежат категории "products"
         for (Outcome outcome : products) {
