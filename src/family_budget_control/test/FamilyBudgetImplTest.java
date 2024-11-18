2f8a741a8bd3dd3c69c6ca86c3fecf4a8f346c7d
german <german.kisin.box@gmail.com>
2024-11-18 16:23:47 +0100

    Test changed1

--- src/family_budget_control/test/FamilyBudgetImplTest.java
+++ src/family_budget_control/test/FamilyBudgetImplTest.java
@@ -1,14 +1,32 @@
 package family_budget_control.test;
 
+import family_budget_control.dao.FamilyBudget;
+import family_budget_control.model.Source;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-class FamilyBudgetImplTest {
 
+class FamilyBudgetImplTest {
+    FamilyBudget familyBudget;
+    LocalDate now = LocalDate.now();
     @BeforeEach
     void setUp() {
+        Source source1 = new Source("products", "rewe", 27.50);
+        Source source2 = new Source("transport", "aral", 27.50);
+        Source source3 = new Source("products", "aldi", 27.50);
+        Source source4 = new Source("communications", "vodafone", 27.50);
+        Source source5 = new Source("entertainment", "cinema", 27.50);
+        Source source6 = new Source("transport", "bus", 27.50);
+
+        familyBudget = new FamilyBudgetImpl();
+
+        familyBudget.addOutcome(new Outcome(1, source1, now.minusDays(13)));
+        familyBudget.addOutcome(new Outcome(2, source2, now.minusDays(11)));
+        familyBudget.addOutcome(new Outcome(3, source3, now.minusDays(10)));
+        familyBudget.addOutcome(new Outcome(4, source4, now.minusDays(7)));
+        familyBudget.addOutcome(new Outcome(5, source5, now.minusDays(7)));
+        familyBudget.addOutcome(new Outcome(6, source6, now.minusDays(4)));
     }
 
     @Test
@@ -29,6 +47,15 @@ class FamilyBudgetImplTest {
     @Test
     @DisplayName("")
     void testFindOutcome() {
+        // Проверяем существующую запись
+        Outcome foundOutcome = familyBudget.findOutcome(1); // Найти запись с ID 1
+        assertNotNull(foundOutcome); // Убедиться, что запись найдена
+        assertEquals("products", foundOutcome.getSource().getCategory()); // Проверить категорию
+        assertEquals("rewe", foundOutcome.getSource().getName()); // Проверить название
+        assertEquals(27.50, foundOutcome.getSource().getAmount()); // Проверить сумму
+
+        // Проверяем отсутствие записи
+        assertNull(familyBudget.findOutcome(7)); // Убедиться, что запись с ID 7 отсутствует
     }
 
     @Test
