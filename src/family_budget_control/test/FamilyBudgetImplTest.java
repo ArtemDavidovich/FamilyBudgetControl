591b90f93ab9de55a3bea24c10d880d357d44e55
Cari <carusel.x@gmail.com>
2024-11-18 15:03:55 +0100

    save

---
+++ src/family_budget_control/test/FamilyBudgetImplTest.java
@@ -0,0 +1,73 @@
+package family_budget_control.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class FamilyBudgetImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testAddOutcome() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testRemoveOutcome() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testSearchOutcome() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testFindOutcome() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testUpdateOutcome() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testSaveTasks() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testLoadTasks() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testFindOutcomeByPredicate() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testOutcomeByProduct() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testOutcomeByTransport() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testOutcomeByMobNetworkAndInternet() {
+    }
+
+    @Test
+    @DisplayName("")
+    void testOutcomeByOthers() {
+    }
+}
